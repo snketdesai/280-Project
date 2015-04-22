a6c2e7c7cfde5f23fc90c0dfbf0082e12a2ae07b
snketdesai <snket.desai@gmail.com>
2015-04-22 14:08:53 -0700

    morris

--- public/assets/js/ajaxcallForMorris.js
+++ public/assets/js/ajaxcallForMorris.js
@@ -2,14 +2,207 @@ $(document).ready(function(){
 	
 	generateChart();
 	genrateBTctoUSD();
-	
+	generateMinerRevenue();
+	generateTotalTransactionPerDay();	
 });
 
+function generateTotalTransactionPerDay()
+{
+	var options={
+			
+			chart: {
+				renderTo: 'transaction',
+	            type: 'scatter',
+	            zoomType: 'xy'
+	        },
+	        title: {
+	            text: 'Unique Transaction by Date'
+	        },
+	        subtitle: {
+	            text: 'Source: Heinz  2003'
+	        },
+	        xAxis: {
+	            title: {
+	                enabled: true,
+	                text: 'Date'
+	            },
+	            categories: []
+	           // startOnTick: true,
+	          //  endOnTick: true,
+	           // showLastLabel: true
+	        },
+	        yAxis: {
+	        	min: 0,
+	             max: 200000,
+	             tickInterval: 5000,
+	            title: {
+	                text: 'Unique Transaction'
+	            }
+	        },
+	        legend: {
+	            layout: 'vertical',
+	            align: 'left',
+	            verticalAlign: 'top',
+	          //  x: 100,
+	          //  y: 70,
+	            floating: true,
+	            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+	            borderWidth: 1
+	        },
+	        plotOptions: {
+	            scatter: {
+	                marker: {
+	                    radius: 5,
+	                    states: {
+	                        hover: {
+	                            enabled: true,
+	                            lineColor: 'rgb(100,100,100)'
+	                        }
+	                    }
+	                },
+	                states: {
+	                    hover: {
+	                        marker: {
+	                            enabled: false
+	                        }
+	                    }
+	                },
+	                tooltip: {
+	                    headerFormat: '<b>{series.name}</b><br>',
+	                    pointFormat: ' {point.y} unique transactions'
+	                }
+	            }
+	        },
+	        series: [{
+	            name: 'Transactions',
+	            color: 'rgba(223, 83, 83, .5)',
+	            data: []
+
+	        }]
+	}
+  getDataForTransaction(options);
+}
+
+function getDataForTransaction(options)
+{
+	var json_p = [];
+	$.ajax(
+            {
+    type: "GET",
+    url: "https://www.quandl.com/api/v1/datasets/BCHAIN/NTRAN.json?auth_token=rxC5WhqoTPMsxy5r_xew",
+    dataType: 'json',
+    async: false,
+    data: '{}',
+   
+    success: function(data){
+          
+       // options.series[0].name = 'Revenue';
+    	//alert(data.data[0][1]);
+    	//alert(data.data[0]);
+    //	var temp = [];
+           for(i=0;i<data.data.length;i++)
+           {
+        	//   temp.push(data.data[i]);
+        	   	options.series[0].data.push(data.data[i][1]);
+              //  options.series[0].data.push(parseInt(data.d.results[i].ERLOS)/1000000);
+             
+                json_p.push(data.data[i][0]);
+        	 //  	temp.length=0;
+            
+           }
+         //   options.series[0].minPointLength= 10,
+            options.xAxis.categories = json_p;
+            chart_bar = new Highcharts.Chart(options);
+    }
+        });
+}
+function generateMinerRevenue()
+{
+
+	var options={
+			
+			chart: {
+				renderTo: 'minerRevenue',
+	            type: 'column'
+	        },
+	        title: {
+	            text: 'Latest 10 Revenue by Miners'
+	        },
+	        xAxis: {
+	            categories: []
+	        },
+	      //  min: 100,
+	        yAxis: {
+	        	 min: 0,
+	             max: 2000000,
+	             tickInterval: 500000,
+	            //min: 0,
+	            title: {
+	                text: 'Revenue(K)'
+	            },
+	            labels: {
+	                formatter: function () {
+	                	
+	                return this.value + 'k';
+	                    } 
+	                }
+	        },
+	       
+	        plotOptions: {
+	            column: {
+	                //stacking: 'percent'
+	            }
+	        },
+	        series: [{
+	            name: 'Revenue',
+	        //   colorByPoint: true,
+	            data: []
+	        }]
+
+		    
+	}
+	getDataForMiner(options);
+}
+
+function getDataForMiner(options)
+{
+	var json_c = [];
+	$.ajax(
+            {
+    type: "GET",
+    url: "https://www.quandl.com/api/v1/datasets/BCHAIN/MIREV.json?rows=10&auth_token=rxC5WhqoTPMsxy5r_xew",
+    dataType: 'json',
+    async: false,
+    data: '{}',
+   
+    success: function(data){
+          
+       // options.series[0].name = 'Revenue';
+    	//alert(data.data[0][1]);
+           for(i=0;i<data.data.length-1;i++)
+           {
+        	   
+        	  // options.xAxis.categories.push(data.data[i][0]);
+        	   	options.series[0].data.push(parseFloat(data.data[i][1]));
+              //  options.series[0].data.push(parseInt(data.d.results[i].ERLOS)/1000000);
+             
+                json_c.push(data.data[i][0]);
+            
+            
+           }
+         //   options.series[0].minPointLength= 10,
+            options.xAxis.categories = json_c;
+            chart_bar = new Highcharts.Chart(options);
+    }
+        });
+	
+}
 function generateChart()
 {
 //alert("yes");
 	var options = {
 	        chart: {
+	        	
 	        	renderTo: 'gr',
 	            zoomType: 'x'
 	        },
